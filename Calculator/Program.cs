5963eddc2ac96f076a5225bb0b0acac209a55ecf
Claire Carlotti <Claire.Calotti@softwire.com>
2022-07-05 12:15:21 +0100

    Calculator stage 5

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,36 +8,54 @@
             string repeat = "yes";
             while (repeat != "no")
             {
-                Console.Write("Please choose an operator [+-*/]: ");
-                string op = Console.ReadLine();
-                Console.Write("Please choose a number: ");
-                string inputa = Console.ReadLine();
-                double firstNo = Double.Parse(inputa);
-                Console.Write("Please choose another number: ");
-                string inputb = Console.ReadLine();
-                double secondNo = Double.Parse(inputb);
+                Calculate();
+                Console.Write("Would you like to do another calculation? [type 'no' to exit] ");
+                repeat = Console.ReadLine();
+            }
+        }
+        static void Calculate()
+        {
+            Console.Write("Please choose an operator [+-*/]: ");
+            string op = Console.ReadLine();
+            double x = RequestNumber("How many numbers do you want to " + op + "?");
+            int count = Convert.ToInt32(x);
+            double[] numbers = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = RequestNumber("Please choose a number:");
+            }
+
+            double answer = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
                 switch (op)
                 {
                     case "+":
-                        Console.WriteLine("The solution is {0}.", firstNo + secondNo);
+                        answer += numbers[i];
                         break;
                     case "-":
-                        Console.WriteLine("The solution is {0}.", firstNo - secondNo);
+                        answer -= numbers[i];
                         break;
                     case "*":
-                        Console.WriteLine("The solution is {0}.", firstNo * secondNo);
+                        answer *= numbers[i];
                         break;
                     case "/":
-                        Console.WriteLine("The solution is {0}.", firstNo / secondNo);
+                        answer /= numbers[i];
                         break;
                     default:
                         Console.WriteLine("Invalid operator given");
                         break;
                 }
-
-                Console.Write("Would you like to do another calculation? [yes/no] ");
-                repeat = Console.ReadLine();
             }
+            Console.WriteLine("The solution is {0}.", answer);
+        }
+
+        static double RequestNumber(string text)
+        {
+            Console.Write(text + " ");
+            string input = Console.ReadLine();
+            double n = Double.Parse(input);
+            return n;
         }
     }
 }    
